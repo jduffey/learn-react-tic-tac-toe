ef7de63fd22de2c7c1d0905d2f3ff20b3b090658
Jed Duffey <jedduffey@gmail.com>
2023-01-08 22:50:05 -0500

    Display buttons for resetting game board to previous moves (not yet functional)

--- src/index.js
+++ src/index.js
@@ -77,6 +77,19 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>
+                        {desc}
+                    </button>
+                </li>
+            );
+        })
+
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -94,7 +107,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div className="status">{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol start="0">{moves}</ol>
                 </div>
             </div>
         );
