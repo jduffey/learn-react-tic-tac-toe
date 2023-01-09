8e880dc7a712ecc24e24190d83371178ae67a513
Jed Duffey <jedduffey@gmail.com>
2023-01-09 11:29:02 -0500

    Refactor: extract calculateWinner function to utils directory

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Board } from "./Board";
+import { calculateWinner } from '../utils/calculateWinner';
 
 const [playerOneMark, playerTwoMark] = ['X', 'O'];
 
@@ -83,30 +84,4 @@ export class Game extends React.Component {
             </div>
         );
     }
-}
-
-function calculateWinner(squares) {
-    const winningConfigurations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < winningConfigurations.length; i++) {
-        const [a, b, c] = winningConfigurations[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return {
-                winner: squares[a],
-                winningSquares: [a, b, c],
-            };
-        }
-    }
-    return {
-        winner: null,
-        winningSquares: null,
-    };
 }
