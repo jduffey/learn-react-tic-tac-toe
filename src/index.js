805358a3850912c6fae608480252e963d56d97fb
Jed Duffey <jedduffey@gmail.com>
2023-01-09 10:47:32 -0500

    Refactor: extract Board to components directory

--- src/index.js
+++ src/index.js
@@ -2,43 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { Square } from "./components/Square";
+import { Board } from "./components/Board";
 
 const [playerOneMark, playerTwoMark] = ['X', 'O'];
-
-class Board extends React.Component {
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-                highlight={this.props.winningSquares?.includes(i)}
-            />
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
 
 class Game extends React.Component {
     constructor(props) {
